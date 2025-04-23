ebfed11eb865c422c0d2873557048f13c6826e27
AKELLO-SCOVIA <akelloscovia907@gmail.com>
2025-04-23 12:32:01 +0300

    Update SQL File.sql

--- SQL File.sql
+++ SQL File.sql
@@ -11,5 +11,16 @@ SELECT *
 FROM Finance
 INSERT INTO Finance(StudentName, StudentID,Birthdate, Year, MoblileNo) VALUE
 ('Jane', '235', '1987-1-12','Y1S2','07222455321'),
-('Nicholas','26','2002-7-22',Y1S1',0720458674'),
-('Cyrus','1',1987
+('Nicholas','26','2002-7-22','Y1S1',0720458674'),
+('Cyrus','1',1989-6-15','Y2S1','0704231345'),
+('Neema','2','2008-11-29','Y2S1','0733876987'),
+('Caroline','3','2002-10-18','Y1S1','0732568448'),
+('Judith','29','2003-6-17','Y4S2','0711234567'),
+('Grace','127','2000-1-14','Y1S2'07337659987')
+
+  ii)
+  -- CREARE TABLE Finance(
+  Y1S1,
+  filename VARCHAR(100);
+
+  iii)
