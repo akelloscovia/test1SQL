ce37d48ad5017fdacd8cd7283452e4ac5664babe
A <akelloscovia907@gmail.com>
2025-04-23 11:52:07 +0300

    DBMS

---
+++ SQL File.sql
@@ -0,0 +1,15 @@
+-- CREATE DATABASE test;
+CREATE TABLE Finance (
+StudentName varchar (200),
+StudentID varchar(96),
+year varchar (67),
+Birthdate date,
+MobileNo varchar(100)
+);
+-- SHOW DATABASE
+SELECT *
+FROM Finance
+INSERT INTO Finance(StudentName, StudentID,Birthdate, Year, MoblileNo) VALUE
+('Jane', '235', '1987-1-12','Y1S2','07222455321'),
+('Nicholas','26','2002-7-22',Y1S1',0720458674'),
+('Cyrus','1',1987
